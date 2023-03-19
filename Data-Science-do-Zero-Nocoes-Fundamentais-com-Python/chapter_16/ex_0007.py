6e554097fab1a0f9d3c9377c1eba28552727db65
MarluCruz <65717006+MarluCruz@users.noreply.github.com>
2023-03-19 16:02:48 -0300

    ....

--- Data-Science-do-Zero-Nocoes-Fundamentais-com-Python/chapter_16/ex_0007.py
+++ Data-Science-do-Zero-Nocoes-Fundamentais-com-Python/chapter_16/ex_0007.py
@@ -1,5 +1,63 @@
-from datetime import date
-from ex_0006 import age 
+from datetime import date, timedelta
+from ex_0006 import age
+
+class Poligons:
+    """Cubo = (Year*4) +1 ; Year = (Month31*7) + (Month30*4) + Month28; Month31= 31*dias; Month30 = 30*dia; Month28 = 28*dia; dia = 1"""
+
+def date_to_days(date):
+    thisdict = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    thisdict2 = {1:31, 2:29, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    poligon = poligons()
+    newDate = date
+    days = 0
+    if (date.year/4) <= 1:
+        days += (date.year - 1) * poligon.year
+        if (date.year%4) == 0:
+            for x in range(1, date.month):
+                days += thisdict2.get(x, None)
+        else:
+            for x in range(1, date.month):
+                days += thisdict.get(x, None)
+        days += date.day
+        newDate = timedelta(days)
+        return newDate
+    
+    else:
+        if (date.year % 4) == 0:
+            days += ((date.year / 4)*poligon.cube) - (poligon.year + 1)
+            for x in range(1, date.month):
+                days += thisdict2.get(x, None)
+            days += date.day
+            newDate = timedelta(days)
+            return newDate
+        else: 
+            days += ((int((date.year / 4))*poligon.cube) + ((date.year%4))*poligon.year) - poligon.year
+            for x in range(1, date.month):
+                days += thisdict.get(x, None)
+            days += date.day
+            newDate = timedelta(days)
+            return newDate
+
+def poligons():
+    poligon = Poligons()
+    poligon.day = 1
+    poligon.month28 = 28*poligon.day
+    poligon.month30 = 30*poligon.day
+    poligon.month31 = 31*poligon.day
+    poligon.year = (poligon.month31*7) + (poligon.month30*4) + (poligon.month28)
+    poligon.cube = (poligon.year*4) + 1
+    return poligon
+
+def age_2(birthday1, birthday2):
+
+    thisAge = birthday2.year - birthday1.year
+    if (birthday2.month >= birthday1.month):
+        if (birthday2.month == birthday1.month) and (birthday2.day < birthday1.day):
+            return thisAge -1
+        else:
+            return thisAge
+    else:
+        return thisAge -1
 
 def input_dates():
     
@@ -45,31 +103,98 @@ def doubleDay(birthday1, birthday2):
 
         if birthday1.month > birthday2.month:
             print("O dia duplo é {}/{}/{}!".format(birthday1.year + (diferenca*2), birthday1.month, birthday1.day ))
-            print("O mais velho terá {}".format(age(date(birthday1.year + (diferenca*2), birthday1.month, birthday1.day))))
-            print("O mais novo terá {}".format(age(date(birthday2.year + (diferenca), birthday2.month, birthday2.day))))
+            print("O mais velho terá {}".format(age_2(birthday1, date(birthday1.year + (diferenca*2), birthday1.month, birthday1.day))))
+            print("O mais novo terá {}".format(age_2(birthday2, date(birthday2.year + (diferenca), birthday2.month, birthday2.day))))
 
         if birthday1.month < birthday2.month:
             print("O dia duplo é {}/{}/{}!".format(birthday1.year + (diferenca*2), birthday2.month, birthday2.day))
-            print("O mais velho terá {}".format(age(date(birthday1.year + (diferenca*2), birthday1.month, birthday1.day))))
-            print("O mais novo terá {}".format(age(date(birthday1.year + (diferenca), birthday1.month, birthday1.day))))
+            print("O mais velho terá {}".format(age_2(birthday1, date(birthday1.year + (diferenca*2), birthday1.month, birthday1.day))))
+            print("O mais novo terá {}".format(age_2(birthday2, date( birthday2.year + (diferenca), birthday2.month, birthday2.day))))
 
     elif age1 < age2:
 
         diferenca = age2 - age1
 
         if birthday2.month > birthday1.month:
-            print("O dia duplo é {}/{}/{}!".format(birthday2.year + (diferenca*2)), birthday2.month, birthday2.day)
-            print("O mais velho terá {}".format(age(date(birthday2.year + (diferenca*2), birthday2.month, birthday2.day))))
-            print("O mais novo terá {}".format(age(date(birthday1.year + (diferenca), birthday1.month, birthday1.day))))
+            print("O dia duplo é {}/{}/{}!".format((birthday2.year + (diferenca*2)), birthday2.month, birthday2.day))
+            print("O mais velho terá {}".format(age_2(birthday2, date(birthday2.year + (diferenca*2), birthday2.month, birthday2.day))))
+            print("O mais novo terá {}".format(age_2 (birthday1, date(birthday1.year + (diferenca), birthday1.month, birthday1.day))))
     
         if birthday2.month < birthday1.month:
-            print("O dia duplo é {}/{}/{}!".format(birthday2.year + (diferenca*2)), birthday1.month, birthday1.day)
-            print("O mais velho terá {}".format(age(date(birthday2.year + (diferenca*2), birthday2.month, birthday2.day))))
-            print("O mais novo terá {}".format(age(date(birthday1.year + (diferenca), birthday1.month, birthday1.day))))
+            print("O dia duplo é {}/{}/{}!".format((birthday2.year + (diferenca*2)), birthday1.month, birthday1.day))
+            print("O mais velho terá {}".format(age_2(birthday2, date(birthday2.year + (diferenca*2), birthday2.month, birthday2.day))))
+            print("O mais novo terá {}".format(age_2(birthday1, date(birthday1.year + (diferenca), birthday1.month, birthday1.day))))
     
     elif age1 == age2:
         print("A diferença entre as idades é menor que 1.\n Portanto não existe dia duplo")
         return
+
+def days_to_date(days):
+ 
+    thisdict = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    thisdict2 = {1:31, 2:29, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    poligon = poligons()
+
+    cubes = int(days.days / poligon.cube)
+    if cubes < 1:
+        year = int((days.days - (days.days - int(days.days / poligon.year) * poligon.year))/poligon.year) +1
+        daysOfMonths = (days.days - int(days.days / poligon.year) * poligon.year)
+        count = 0
+        month = 0
+
+        if days.days > poligon.year*3:
+            while (daysOfMonths > 0):
+                count += 1
+                daysOfMonths -= thisdict2.get(count, 0)
+                month += 1
+            day = daysOfMonths + thisdict2.get(count, 0)
+            thisdate = date(year, month, day)
+            return thisdate
+        else:
+            while (daysOfMonths > 0):
+                count += 1
+                daysOfMonths -= thisdict.get(count, 0)
+                month += 1
+            day = daysOfMonths + thisdict.get(count, 0)
+            thisdate = date(year, month, day)
+            return thisdate
+        
+    elif (days.days - ((cubes*poligon.cube) + poligon.year*3)) > 0:
+        anoBisexto = int(((days.days - (days.days - (poligon.cube * cubes))) /poligon.cube) * 4)
+        year = anoBisexto + int((days.days%poligon.cube) /poligon.year) + 1
+        daysOfMonths = days.days - ((poligon.cube*cubes) + ((int((days.days%poligon.cube) / poligon.year))*poligon.year))
+        count = 0
+        month = 0
+        while (daysOfMonths > 0):
+            count += 1
+            daysOfMonths -= thisdict2.get(count, 0)
+            month += 1
+        day = daysOfMonths + thisdict2.get(count, 0)
+        thisdate = date(year, month, day)
+        return thisdate
+    else:
+        anoBisexto = int(((days.days - (days.days - (poligon.cube * cubes))) /poligon.cube) * 4)
+        year = anoBisexto + int((days.days%poligon.cube) /poligon.year) + 1
+   
+        daysOfMonths = days.days - ((poligon.cube*cubes) + (int((days.days%poligon.cube) /poligon.year)* poligon.year)) 
+        count = 0
+        month = 0
+        while (daysOfMonths > 0):
+            count += 1
+            daysOfMonths -= thisdict.get(count, 0)
+            month += 1
+        day = daysOfMonths + thisdict.get(count, 0)
+        thisdate = date(year, month, day)
+        return thisdate
+    
+def doubleDay2(birthday1, birthday2):
+    birthday1_to_days = date_to_days(birthday1)
+    birthday2_to_days = date_to_days(birthday2)
+    if birthday1_to_days > birthday2_to_days:
+        diference = birthday1_to_days - birthday2_to_days
+
+        print('O dia duplo será {}'.format(days_to_date(birthday1_to_days+diference)))
+
 
 def main():
     print("Vamos escolher o ano da primeira data de nascimento:")
@@ -80,7 +205,7 @@ def main():
     birthday2 = input_dates()
     print("_____________________")
     print()
-    doubleDay(birthday1, birthday2)
+    doubleDay2(birthday1, birthday2)
 
 if __name__ == "__main__":
     main()
--- Data-Science-do-Zero-Nocoes-Fundamentais-com-Python/chapter_16/teste.py
+++ Data-Science-do-Zero-Nocoes-Fundamentais-com-Python/chapter_16/teste.py
@@ -1,1 +1,9 @@
-print(1998%4)
+from ex_0007 import Poligons, poligons, date_to_days, days_to_date
+from datetime import timedelta, date
+
+
+
+dateindays = date_to_days(date(2010,6,11))
+
+daysindate = days_to_date(dateindays)
+print(daysindate)
